34f2a513ac7f6212d2db38bef7fc8328e9f3bb0a
Andy Mast <andy@cyngn.com>
2014-11-25 07:26:26 -0800

    Android.mk for gmock
    Change-Id: I75ba622106ec8cee37b5375cbfd02ebe9e71ca75

---
+++ Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libgmock
+
+LOCAL_SRC_FILES := src/gmock-all.cc
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/gmock/internal \
+                    $(LOCAL_PATH)/include
+
+include $(BUILD_HOST_STATIC_LIBRARY)
